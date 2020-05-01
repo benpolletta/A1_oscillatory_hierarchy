81f0705e12d193445bb9a50dba7d36bdf827f346
benpolletta <benpolletta@gmail.com>
2020-05-01 13:29:44 -0400

    freq_ratio: debugging.

--- Internal_Functions/freq_ratio.m
+++ Internal_Functions/freq_ratio.m
@@ -4,10 +4,6 @@ spike_field = 'RS_V_spikes';
 
 input_field = 'RS_iPeriodicSpikes_input';
 
-figure_flag = 0;
-
-no_i_cycles = 1;
-
 if ~isempty(varargin)
     
     for v = 1:(length(varargin)/2)
@@ -21,14 +17,6 @@ if ~isempty(varargin)
             case 'input_field'
                 
                 input_field = varargin{2*v};
-                
-            case 'figure_flag'
-                
-                figure_flag = varargin{2*v};
-                
-            case 'no_periods'
-                
-                no_i_cycles = varargin{2*v};
                 
         end
         
@@ -48,8 +36,8 @@ v_cycle_lengths = diff(v_spike_times);
 v_freqs = 1000./v_cycle_lengths;
 no_v_cycles = length(v_freqs);
 
-[~, input_spike_indices] = findpeaks(diff(i), 'MinPeakHeight', 5*10^-3);
-i_spike_times = t(input_spike_indices);
+[~, input_spike_indices] = findpeaks(diff(i), 'MinPeakHeight', .25*max(diff(i)));
+i_spike_times = [t(input_spike_indices); t(end)];
 i_cycle_lengths = diff(i_spike_times);
 i_freqs = 1000./i_cycle_lengths;
 no_i_cycles = length(i_freqs);
@@ -58,15 +46,15 @@ freq_ratio = nan(size(t));
 
 for p = 1:no_i_cycles
     
-    v_spike_times_pd = v_spike_times(v_spike_times > i_spike_times(p) & v_spike_times <= i_spike_times(p + 1));
+    t_pd = t >= i_spike_times(p) & t <= i_spike_times(p + 1);
     
-    t_pd = t > v_spike_times_pd(1) & t <= v_spike_times_pd(end);
+    v_spikes_pd = v_spikes & t_pd;
     
-    no_spikes_pd = length(v_spike_times_pd);
+    v_spike_times_pd = t(v_spikes_pd);
     
-    ISIs = diff(v_spike_times_pd); % In milliseconds.
+    ISIs = [diff(v_spike_times_pd); nan]; % In milliseconds.
     
-    freq_ratio(v_spikes & t_pd) = (1000./ISIs)/i_freqs(p); % Transform to frequencies.
+    freq_ratio(v_spikes_pd) = (1000./ISIs)/i_freqs(p); % Transform to frequencies.
     
 end
 
